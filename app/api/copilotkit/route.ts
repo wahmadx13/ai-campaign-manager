ca57f4900f4f2283fc8be57f4391464f61344014
Waseem <wasim_ahmad2003@yahoo.com>
2024-04-26 21:50:03 +0500

    add api for opena ai model

---
+++ app/api/copilotkit/route.ts
@@ -0,0 +1,15 @@
+import { CopilotBackend, OpenAIAdapter } from "@copilotkit/backend";
+
+export const runtime = "edge";
+
+export async function POST(req: Request): Promise<Response> {
+  const copilotKit = new CopilotBackend({});
+  const openaiModel = process.env.OPENAI_MODEL;
+
+  return copilotKit.response(
+    req,
+    new OpenAIAdapter({
+      model: openaiModel,
+    })
+  );
+}
